7422675d3d4652c1dae1016463c248771a3dbe75
JonathanArnould <jonathan.arnould33@gmail.com>
2021-11-15 10:00:52 +0100

    button: add console.log to qualify as a button

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,8 @@ module.exports = {
     extends: [
         "@commitlint/config-conventional"
     ],
+    // create custom commit-lint rules, for example add a "button" type (ex: "button(ui): .....")
+    // more informations here: https://commitlint.js.org/#/reference-rules
     rules: {
         "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "button"]],
     }
